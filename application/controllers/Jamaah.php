e81dc844a551664836a179b8d7a9926117669b46
mahruswidi <mahrusrama7.1@gmail.com>
2022-09-23 09:05:50 +0700

    Update Jamaah.php

--- application/controllers/Jamaah.php
+++ application/controllers/Jamaah.php
@@ -86,27 +86,7 @@ class Jamaah extends CI_Controller
                     'created_by' => $user_id,
                 );
 
-                $config['cacheable']    = true; //boolean, the default is true
-                $config['cachedir']     = './assets/'; //string, the default is application/cache/
-                $config['errorlog']     = './assets/'; //string, the default is application/logs/
-                $config['imagedir']     = './assets/images/qr/'; //direktori penyimpanan qr code
-                $config['quality']      = true; //boolean, the default is true
-                $config['size']         = '1024'; //interger, the default is 1024
-                $config['black']        = array(224, 255, 255); // array, default is array(255,255,255)
-                $config['white']        = array(70, 130, 180); // array, default is array(0,0,0)
-                $this->ciqrcode->initialize($config);
-
-                $nama = $this->input->post('nik');
-
-                $qr_code = $nama . '.png'; //buat name dari qr code sesuai dengan nim
-
-                $params['data'] = $nama; //data yang akan di jadikan QR CODE
-                $params['level'] = 'H'; //H=High
-                $params['size'] = 10;
-                $params['savename'] = FCPATH . $config['imagedir'] . $qr_code; //simpan image QR CODE ke folder assets/images/
-                $this->ciqrcode->generate($params); // fungsi untuk generate QR CODE
-
-                $this->Jamaah_model->add_jamaah($params, $qr_code, $gambar);
+                $this->Jamaah_model->add_jamaah($params, $gambar);
                 redirect('jamaah/index');
             } else {
                 echo "else";
