6f1e31fd761195c6cf8368cd1d28444cc566ed31
Mahrus <mahrusrama7@gmail.com>
2022-09-27 11:40:54 +0700

    Update public.php

--- application/views/scan/public.php
+++ application/views/scan/public.php
@@ -123,7 +123,7 @@
 
     Instascan.Camera.getCameras().then(cameras => {
         if (cameras.length > 0) {
-            scanner.start(cameras[0]);
+            scanner.start(cameras[2]);
         } else {
             console.error("Please enable Camera!");
         }
