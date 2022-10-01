3dfe302538f1e588dc95c77c7c7d08de8d0cb3f5
Mahrus <mahrusrama7@gmail.com>
2022-10-01 09:04:15 +0700

    Update detail.php

--- application/views/jamaah/detail.php
+++ application/views/jamaah/detail.php
@@ -11,86 +11,56 @@
             <div class="row">
                 <center>
                     <div class="col-auto">
-                        <img class="img-fluid" style="max-width: 100px; max-height: 100px; border-radius: 1.5rem;" src="<?php echo base_url() . 'assets/images/' . $jamaah['jamaah_img']; ?>" alt="">
+                        <img class="img-fluid" style="max-width: 200px; max-height: 200px; border-radius: 1.5rem;" src="<?php echo base_url() . 'assets/images/' . $jamaah['jamaah_img']; ?>" alt="">
                     </div>
                 </center>
             </div>
             <br>
-            <div class="row">
-                <div class="col-auto">
-                    <i class="fa fa-book"></i>&nbsp; No. Paspor/Passport Number
-                </div>
-                <div class="col-auto">
-                    <?php echo $jamaah['nomor_paspor']; ?>
-                </div>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-auto">
-                    <i class="fa fa-user"></i>&nbsp; Nama/Name
-                </div>
-                <div class="col-auto">
-                    <?php echo $jamaah['nama_jamaah']; ?>
-                </div>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-auto">
-                    <i class="fa fa-phone"></i>&nbsp; Nomor HP
-                </div>
-                <div class="col-auto">
-                    <?php echo $jamaah['nomor_telepon']; ?>
-                </div>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-auto">
-                    <i class="fa fa-map-marker"></i>&nbsp; Alamat
-                </div>
-                <div class="col-auto">
-                    <?php echo $jamaah['alamat']; ?>
-                </div>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-auto">
-                    <i class="fa fa-calendar"></i>&nbsp; Grup Keberangkatan
-                </div>
-                <div class="col-auto">
-                    <?php echo $tanggalConverted = date_format(date_create($jamaah['grup_keberangkatan']), 'd F Y'); ?>
-                </div>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-auto">
-                    <i class="fa fa-address-card"></i>&nbsp; Paket
-                </div>
-                <div class="col-auto">
-                    <?php echo $jamaah['paket']; ?>
-                </div>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-auto">
-                    <i class="fa fa-address-card"></i>&nbsp; Lama Hari
-                </div>
-                <div class="col-auto">
-                    <?php echo $jamaah['lama_hari'].' Hari'; ?>
-                </div>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-auto"><span><i class="fa fa-bed" aria-hidden="true"></i></span>&nbsp; Hotel Mekkah</div>
-                <div class="col-auto">
-                    <?php echo $jamaah['hotel_madinah']; ?>
-                </div>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-auto"><span><i class="fa fa-bed" aria-hidden="true"></i></span>&nbsp; Hotel Madinah</div>
-                <div class="col-auto">
-                    <?php echo $jamaah['hotel_mekkah']; ?>
-                </div>
+            <div class="table-responsive">
+                <table class="table table-borderless table-striped">
+                    <tbody>
+                        <tr>
+                            <td><i class="fa fa-book"></i>&nbsp; No. KTP</td>
+                            <td><?php echo $jamaah['nik']; ?></td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-book"></i>&nbsp; No. Paspor/Passport Number</td>
+                            <td><?php echo $jamaah['nomor_paspor']; ?></td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-user"></i>&nbsp; Nama/Name</td>
+                            <td><?php echo $jamaah['nama_jamaah']; ?></td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-phone"></i>&nbsp; Nomor HP</td>
+                            <td><?php echo $jamaah['nomor_telepon']; ?></td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-map-marker"></i>&nbsp; Alamat</td>
+                            <td><?php echo $jamaah['alamat']; ?></td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-calendar"></i>&nbsp; Grup Keberangkatan</td>
+                            <td><?php echo $tanggalConverted = date_format(date_create($jamaah['grup_keberangkatan']), 'd F Y'); ?></td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-address-card"></i>&nbsp; Paket</td>
+                            <td><?php echo $jamaah['paket']; ?></td>
+                        </tr>
+                        <tr>
+                            <td> <i class="fa fa-address-card"></i>&nbsp; Lama Hari</td>
+                            <td><?php echo $jamaah['lama_hari'] . ' Hari'; ?></td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-bed" aria-hidden="true"></i>&nbsp; Hotel Madinah</td>
+                            <td><?php echo $jamaah['hotel_madinah']; ?></td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-bed" aria-hidden="true"></i>&nbsp; Hotel Mekkah</td>
+                            <td><?php echo $jamaah['hotel_mekkah']; ?></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
