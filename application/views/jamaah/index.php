cd05c008288cf2b9b54bc02701b2f3d89a2ae223
Mahrus <mahrusrama7@gmail.com>
2022-10-05 15:34:26 +0700

    Update index.php

--- application/views/jamaah/index.php
+++ application/views/jamaah/index.php
@@ -137,7 +137,7 @@
             "serverSide": true,
             "ordering": true, // Set true agar bisa di sorting
             "order": [
-                [0, 'desc']
+                [7, 'desc']
             ], // Default sortingnya berdasarkan kolom / field ke 0 (paling pertama)
             "ajax": {
                 "url": "<?php echo site_url('jamaah/view') ?>", // URL file untuk proses select datanya
