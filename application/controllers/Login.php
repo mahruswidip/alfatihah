5dd49a6c2bd9b22959ee739d8a29f45ecda105b3
Mahrus <mahrusrama7@gmail.com>
2022-11-18 10:51:11 +0700

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -98,9 +98,9 @@ class Login extends CI_Controller
     $dataLogin = $this->db->query("SELECT * FROM tbl_users where user_email = '" . $email . "' AND user_password = '" . $password . "'");
   
     foreach ($dataLogin->result() as $dl) {
-      $dataAdmin[] = $dl;
+      $dataAdmin = $dl;
     }
-    if ($dataAdmin == []) {
+    if ($dataAdmin == '') {
       http_response_code(500);
       $dataAdmin = "Data Anda Tidak Ditemukan";
       echo json_encode($dataAdmin);
@@ -108,5 +108,9 @@ class Login extends CI_Controller
       echo json_encode($dataAdmin);
     }
     
+  }
+  public function get_user()
+  {
+    # code...
   }
 }
