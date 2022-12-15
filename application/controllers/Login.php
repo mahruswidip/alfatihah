a983308a29966e836d360469ee9639f96cace16c
Mahrus <mahrusrama7@gmail.com>
2022-12-15 16:10:44 +0700

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -116,16 +116,13 @@ class Login extends CI_Controller
 
     // $cek = $this->db->query("SELECT * FROM tbl_users WHERE `user_email` = '" . $user_email . "' ");
 
-    global $mysqli;
     $query = "SELECT * FROM tbl_users";
     if ($user_email != 0) {
       $query .= " WHERE user_email=" . $user_email . " LIMIT 1";
     }
     $data = array();
-    $result = $mysqli->query($query);
-    while ($row = mysqli_fetch_object($result)) {
-      $data[] = $row;
-    }
+    $result = $this->db->query($query);
+    mysqli_fetch_array($result);
     $response = array(
       'status' => 1,
       'message' => 'Get Jamaah Successfully.',
