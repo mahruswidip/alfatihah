37168b653d00c52e2ef49a3b84505e070068cc12
Mahrus <mahrusrama7@gmail.com>
2022-12-15 15:18:44 +0700

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -116,6 +116,15 @@ class Login extends CI_Controller
 
     $dataLupaPassword = $this->db->query("UPDATE tbl_users SET `pass` = '" . $user_password . "', user_password = md5('" . $user_password . "')  WHERE `user_email` = '" . $user_email . "'");
 
-    return $dataLupaPassword;
+    foreach ($dataLupaPassword->result() as $dl) {
+      $dataLupaPassword = $dl;
+    }
+    if ($dataLupaPassword == []) {
+      http_response_code(404);
+      $dataLupaPassword = "Data Anda Tidak Ditemukan";
+      echo json_encode($dataLupaPassword);
+    } else {
+      echo json_encode($dataLupaPassword);
+    }
   }
 }
